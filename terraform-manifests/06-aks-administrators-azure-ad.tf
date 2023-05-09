6b2a4c33698d3c4b5d8a52641c50604267d0a370
rajeshmgc <rajeshmgc@gmail.com>
2023-05-09 12:02:50 -0400

    Changes for Latest versionb Commit

--- terraform-manifests/06-aks-administrators-azure-ad.tf
+++ terraform-manifests/06-aks-administrators-azure-ad.tf
@@ -5,3 +5,27 @@ resource "azuread_group" "aks_administrators" {
   description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-${var.environment} cluster."
   security_enabled = true
 }
+
+# Create Azure AD Group in Active Directory for AKS Admins
+resource "azuread_group" "aks_administrators" {
+  display_name        = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
+  description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-${var.environment} cluster."
+}
+
+# Create an Azure Active Directory user
+resource "azuread_user" "k8s_user" {
+  display_name = "aksdev2"
+  password     = "MyStrongPassword123!"
+  user_principal_name = "aksdev2@rajeshmgcoutlook.onmicrosoft.com"
+}
+
+resource "azuread_group_member" "admin_group_member" {
+  group_object_id = azuread_group.aks_administrators.id
+  member_object_id = azuread_user.k8s_user.id
+}
+
+resource "azurerm_role_assignment" "k8-admins" {
+  scope = azurerm_kubernetes_cluster.aks_cluster.id
+  role_definition_id  = "/subscriptions/9d9fcbde-f936-44c8-aadf-33737c6f78a4/providers/Microsoft.Authorization/roleDefinitions/b1ff04bb-8a4e-4dc4-8eb5-8693973ce19b"
+  principal_id = azuread_group.aks_administrators.id
+}
--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -35,16 +35,20 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   identity { type = "SystemAssigned" }
 
 # Add On Profiles
-  addon_profile {
-    azure_policy { enabled = true }
-    oms_agent {
-      enabled                    = true
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
-    }
+  oms_agent {
+    
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
   }
 
 # RBAC and Azure AD Integration Block
-role_based_access_control {
+  azure_active_directory_role_based_access_control {
+    managed = true
+    admin_group_object_ids=[azuread_group.aks_administrators.id]
+    azure_rbac_enabled= true
+  }
+
+# RBAC and Azure AD Integration Block
+  role_based_access_control {
   enabled = true
   azure_active_directory {
     managed                = true
@@ -53,13 +57,13 @@ role_based_access_control {
 }  
 
 # Windows Admin Profile
-windows_profile {
-  admin_username            = var.windows_admin_username
-  admin_password            = var.windows_admin_password
+  windows_profile {
+    admin_username            = var.windows_admin_username
+    admin_password            = var.windows_admin_password
 }
 
 # Linux Profile
-linux_profile {
+  linux_profile {
   admin_username = "ubuntu"
   ssh_key {
       key_data = file(var.ssh_public_key)
@@ -67,15 +71,14 @@ linux_profile {
 }
 
 # Network Profile
-network_profile {
-  load_balancer_sku = "Standard"
-  network_plugin = "azure"
+  network_profile {
+    load_balancer_sku = "Standard"
+    network_plugin = "azure"
 }
 
 # AKS Cluster Tags 
-tags = {
-  Environment = var.environment
+  tags = {
+    Environment = var.environment
 }
-
 
 }
