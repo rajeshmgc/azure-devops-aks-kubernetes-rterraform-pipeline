71e656980d8607597f7999d6cdf39443cf594480
rajeshmgc <rajeshmgc@gmail.com>
2023-05-09 13:33:26 -0400

    Changes to fix the issue in AdGroup Displayname and cluster

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -47,13 +47,6 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     azure_rbac_enabled= true
   }
 
-# RBAC and Azure AD Integration Block
-  role_based_access_control {
-  enabled = true
-  azure_active_directory {
-    managed                = true
-    admin_group_object_ids = [azuread_group.aks_administrators.id]
-  }
 }  
 
 # Windows Admin Profile
