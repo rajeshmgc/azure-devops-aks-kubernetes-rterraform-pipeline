e0863c726c02eb42460f579f036b9954d78baf07
rajeshmgc <rajeshmgc@gmail.com>
2023-05-09 12:06:17 -0400

    Changes for Latest versionn Commit

--- terraform-manifests/01-main.tf
+++ terraform-manifests/01-main.tf
@@ -26,7 +26,7 @@ terraform {
     }
   }
 
-# Terraform State Storage to Azure Storage Container
+# Terraform State Storage to Azure Storage Container commented
   backend "azurerm" {
     #resource_group_name   = "terraform-storage-rg"
     #storage_account_name  = "terraformstatexlrwdrzs"
