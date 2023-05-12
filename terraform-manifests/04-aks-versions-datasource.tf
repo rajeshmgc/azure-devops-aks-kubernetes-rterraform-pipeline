f52c934251b0a10459928bf4c345276a13ea6a6e
rajeshmgc <rajeshmgc@gmail.com>
2023-05-12 11:10:49 -0400

    Changed the enduser name for QA

--- terraform-manifests/04-aks-versions-datasource.tf
+++ terraform-manifests/04-aks-versions-datasource.tf
@@ -5,5 +5,3 @@ data "azurerm_kubernetes_service_versions" "current" {
   include_preview = false
 }
 
-
-
--- terraform-manifests/06-aks-administrators-azure-ad.tf
+++ terraform-manifests/06-aks-administrators-azure-ad.tf
@@ -13,7 +13,7 @@ resource "azuread_group" "aks_administrators" {
 resource "azuread_user" "k8s_user" {
   display_name = "aksdev2"
   password     = "MyStrongPassword@123"
-  user_principal_name = "aksdev2@rajeshmgcoutlook.onmicrosoft.com"
+  user_principal_name = "${azurerm_resource_group.aks_rg.name}-user@rajeshmgcoutlook.onmicrosoft.com"
 }
 
 /* resource "azuread_group_member" "admin_group_member" {
@@ -21,8 +21,16 @@ resource "azuread_user" "k8s_user" {
   member_object_id = azuread_user.k8s_user.object_id
 } */
 
+
+
 resource "azurerm_role_assignment" "k8-admins" {
   scope = azurerm_kubernetes_cluster.aks_cluster.id
   role_definition_id  = "/subscriptions/9d9fcbde-f936-44c8-aadf-33737c6f78a4/providers/Microsoft.Authorization/roleDefinitions/b1ff04bb-8a4e-4dc4-8eb5-8693973ce19b"
   principal_id = azuread_group.aks_administrators.id
 }
+
+
+#https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/data-sources/service_principal
+/* data "azuread_service_principal" "example" {
+  display_name = "my-awesome-application"
+} */
